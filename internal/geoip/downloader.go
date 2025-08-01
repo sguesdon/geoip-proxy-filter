24a591d58c301723f2fe416aa6e538d8746e4b4d
Sebastian Guesdon <sebastian.guesdon@gmail.com>
2025-08-01 16:06:17 +0200

    chore: lint files

--- internal/geoip/downloader.go
+++ internal/geoip/downloader.go
@@ -13,106 +13,106 @@ import (
 )
 
 type Downloader struct {
-    config *config.Config
+	config *config.Config
 }
 
 func NewDownloader(config *config.Config) *Downloader {
-    return &Downloader{
-        config: config,
-    }
+	return &Downloader{
+		config: config,
+	}
 }
 
 func (d *Downloader) UpdateDatabase() error {
-    if d.config.MaxMind.AccountID == "" || d.config.MaxMind.LicenseKey == "" {
-        return fmt.Errorf("GEOLITE_ACCOUNT_ID and GEOLITE_LICENCE_KEY environment variables must be set")
-    }
+	if d.config.MaxMind.AccountID == "" || d.config.MaxMind.LicenseKey == "" {
+		return fmt.Errorf("GEOLITE_ACCOUNT_ID and GEOLITE_LICENCE_KEY environment variables must be set")
+	}
 
-    // Download the tar.gz file
-    url := fmt.Sprintf(d.config.MaxMind.URL, d.config.MaxMind.LicenseKey)
-    tarFilename := d.config.TmpDir + d.config.MaxMind.GeolitePrefix + "_latest.tar.gz"
+	// Download the tar.gz file
+	url := fmt.Sprintf(d.config.MaxMind.URL, d.config.MaxMind.LicenseKey)
+	tarFilename := d.config.TmpDir + d.config.MaxMind.GeolitePrefix + "_latest.tar.gz"
 
-    if err := d.downloadFile(url, tarFilename); err != nil {
-        return fmt.Errorf("failed to download file: %w", err)
-    }
-    defer os.Remove(tarFilename)
+	if err := d.downloadFile(url, tarFilename); err != nil {
+		return fmt.Errorf("failed to download file: %w", err)
+	}
+	defer os.Remove(tarFilename)
 
-    // Extract the .mmdb file
-    mmdbFilename := d.config.MaxMind.GeolitePrefix + ".mmdb"
-    if err := d.extractMMDBFile(tarFilename, mmdbFilename); err != nil {
-        return fmt.Errorf("failed to extract file: %w", err)
-    }
+	// Extract the .mmdb file
+	mmdbFilename := d.config.MaxMind.GeolitePrefix + ".mmdb"
+	if err := d.extractMMDBFile(tarFilename, mmdbFilename); err != nil {
+		return fmt.Errorf("failed to extract file: %w", err)
+	}
 
-    if err := os.Rename(mmdbFilename, d.config.GeoIP.DatabasePath); err != nil {
-        return fmt.Errorf("failed to move file to location: %w", err)
-    }
+	if err := os.Rename(mmdbFilename, d.config.GeoIP.DatabasePath); err != nil {
+		return fmt.Errorf("failed to move file to location: %w", err)
+	}
 
-    return nil
+	return nil
 }
 
 func (d *Downloader) downloadFile(url, filename string) error {
-    client := &http.Client{}
-    req, err := http.NewRequest("GET", url, nil)
-    if err != nil {
-        return err
-    }
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
 
-    req.SetBasicAuth(d.config.MaxMind.AccountID, d.config.MaxMind.LicenseKey)
+	req.SetBasicAuth(d.config.MaxMind.AccountID, d.config.MaxMind.LicenseKey)
 
-    resp, err := client.Do(req)
-    if err != nil {
-        return err
-    }
-    defer resp.Body.Close()
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-    if resp.StatusCode != http.StatusOK {
-        return fmt.Errorf("bad status: %s", resp.Status)
-    }
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
 
-    out, err := os.Create(filename)
-    if err != nil {
-        return err
-    }
-    defer out.Close()
+	out, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
 
-    _, err = io.Copy(out, resp.Body)
-    return err
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
 
 func (d *Downloader) extractMMDBFile(tarFilename, mmdbFilename string) error {
-    file, err := os.Open(tarFilename)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
+	file, err := os.Open(tarFilename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-    gzr, err := gzip.NewReader(file)
-    if err != nil {
-        return err
-    }
-    defer gzr.Close()
+	gzr, err := gzip.NewReader(file)
+	if err != nil {
+		return err
+	}
+	defer gzr.Close()
 
-    tr := tar.NewReader(gzr)
+	tr := tar.NewReader(gzr)
 
-    for {
-        header, err := tr.Next()
-        if err == io.EOF {
-            break
-        }
-        if err != nil {
-            return err
-        }
+	for {
+		header, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
 
-        if strings.HasSuffix(header.Name, ".mmdb") {
-            outFile, err := os.Create(mmdbFilename)
-            if err != nil {
-                return err
-            }
-            defer outFile.Close()
+		if strings.HasSuffix(header.Name, ".mmdb") {
+			outFile, err := os.Create(mmdbFilename)
+			if err != nil {
+				return err
+			}
+			defer outFile.Close()
 
-            _, err = io.Copy(outFile, tr)
-            return err
-        }
-    }
+			_, err = io.Copy(outFile, tr)
+			return err
+		}
+	}
 
-    return fmt.Errorf("no .mmdb file found in archive")
+	return fmt.Errorf("no .mmdb file found in archive")
 }
--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -8,103 +8,103 @@ import (
 )
 
 var (
-    // Compteurs de requêtes
-    RequestsTotal = promauto.NewCounterVec(
-        prometheus.CounterOpts{
-            Name: "geoip_proxy_requests_total",
-            Help: "Total number of proxy requests",
-        },
-        []string{"method", "country", "action"}, // action: allowed/blocked
-    )
+	// Compteurs de requêtes
+	RequestsTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "geoip_proxy_requests_total",
+			Help: "Total number of proxy requests",
+		},
+		[]string{"method", "country", "action"}, // action: allowed/blocked
+	)
 
-    // Durée des lookups GeoIP
-    GeoIPLookupDuration = promauto.NewHistogram(
-        prometheus.HistogramOpts{
-            Name:    "geoip_lookup_duration_seconds",
-            Help:    "Time spent on GeoIP lookups",
-            Buckets: prometheus.DefBuckets,
-        },
-    )
+	// Durée des lookups GeoIP
+	GeoIPLookupDuration = promauto.NewHistogram(
+		prometheus.HistogramOpts{
+			Name:    "geoip_lookup_duration_seconds",
+			Help:    "Time spent on GeoIP lookups",
+			Buckets: prometheus.DefBuckets,
+		},
+	)
 
-    // Statut de la base GeoIP
-    GeoIPDatabaseStatus = promauto.NewGauge(
-        prometheus.GaugeOpts{
-            Name: "geoip_database_status",
-            Help: "Status of GeoIP database (1=healthy, 0=unhealthy)",
-        },
-    )
+	// Statut de la base GeoIP
+	GeoIPDatabaseStatus = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "geoip_database_status",
+			Help: "Status of GeoIP database (1=healthy, 0=unhealthy)",
+		},
+	)
 
-    // Timestamp de la dernière mise à jour de la DB
-    GeoIPDatabaseLastUpdate = promauto.NewGauge(
-        prometheus.GaugeOpts{
-            Name: "geoip_database_last_update_timestamp",
-            Help: "Timestamp of last GeoIP database update",
-        },
-    )
+	// Timestamp de la dernière mise à jour de la DB
+	GeoIPDatabaseLastUpdate = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "geoip_database_last_update_timestamp",
+			Help: "Timestamp of last GeoIP database update",
+		},
+	)
 
-    // Durée des requêtes proxy
-    ProxyRequestDuration = promauto.NewHistogramVec(
-        prometheus.HistogramOpts{
-            Name:    "geoip_proxy_request_duration_seconds",
-            Help:    "Time spent processing proxy requests",
-            Buckets: []float64{0.1, 0.5, 1.0, 2.5, 5.0, 10.0},
-        },
-        []string{"method"},
-    )
+	// Durée des requêtes proxy
+	ProxyRequestDuration = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "geoip_proxy_request_duration_seconds",
+			Help:    "Time spent processing proxy requests",
+			Buckets: []float64{0.1, 0.5, 1.0, 2.5, 5.0, 10.0},
+		},
+		[]string{"method"},
+	)
 
-    // Connexions actives
-    ActiveConnections = promauto.NewGauge(
-        prometheus.GaugeOpts{
-            Name: "geoip_proxy_active_connections",
-            Help: "Number of active proxy connections",
-        },
-    )
+	// Connexions actives
+	ActiveConnections = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "geoip_proxy_active_connections",
+			Help: "Number of active proxy connections",
+		},
+	)
 
-    // Erreurs de résolution DNS
-    DNSResolutionErrors = promauto.NewCounter(
-        prometheus.CounterOpts{
-            Name: "geoip_proxy_dns_resolution_errors_total",
-            Help: "Total number of DNS resolution errors",
-        },
-    )
+	// Erreurs de résolution DNS
+	DNSResolutionErrors = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Name: "geoip_proxy_dns_resolution_errors_total",
+			Help: "Total number of DNS resolution errors",
+		},
+	)
 )
 
 // RecordGeoIPLookup enregistre une métrique de lookup GeoIP
 func RecordGeoIPLookup(duration time.Duration) {
-    GeoIPLookupDuration.Observe(duration.Seconds())
+	GeoIPLookupDuration.Observe(duration.Seconds())
 }
 
 // RecordRequest enregistre une métrique de requête
 func RecordRequest(method, country, action string) {
-    RequestsTotal.WithLabelValues(method, country, action).Inc()
+	RequestsTotal.WithLabelValues(method, country, action).Inc()
 }
 
 // RecordProxyDuration enregistre la durée d'une requête proxy
 func RecordProxyDuration(method string, duration time.Duration) {
-    ProxyRequestDuration.WithLabelValues(method).Observe(duration.Seconds())
+	ProxyRequestDuration.WithLabelValues(method).Observe(duration.Seconds())
 }
 
 // UpdateDatabaseStatus met à jour le statut de la base GeoIP
 func UpdateDatabaseStatus(healthy bool) {
-    if healthy {
-        GeoIPDatabaseStatus.Set(1)
-        GeoIPDatabaseLastUpdate.SetToCurrentTime()
-    } else {
-        GeoIPDatabaseStatus.Set(0)
-    }
+	if healthy {
+		GeoIPDatabaseStatus.Set(1)
+		GeoIPDatabaseLastUpdate.SetToCurrentTime()
+	} else {
+		GeoIPDatabaseStatus.Set(0)
+	}
 }
 
 // IncrementActiveConnections incrémente le compteur de connexions actives
 func IncrementActiveConnections() {
-    ActiveConnections.Inc()
+	ActiveConnections.Inc()
 }
 
 // DecrementActiveConnections décrémente le compteur de connexions actives
 func DecrementActiveConnections() {
-    ActiveConnections.Dec()
+	ActiveConnections.Dec()
 }
 
 // RecordDNSError enregistre une erreur DNS
 func RecordDNSError() {
-    DNSResolutionErrors.Inc()
+	DNSResolutionErrors.Inc()
 }
